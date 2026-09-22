40cd02c0b4957b727e8fbdc0b6354e4708074017
AdilKhan-tech <adil.ce2007@gmail.com>
2026-09-22 17:21:16 +0500

    c

--- marble/app/(frontend)/contact/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Contact from '../../../components/frontend/contact/Contact'
-function Page() {
-  return (
-    <div><Contact /></div>
-  )
-}
-
-export default Page
